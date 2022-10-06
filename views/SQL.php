76046f62374235acd5b3af7dbd8780d80bef5d67
sari7happy <sari7happy@gmail.com>
2022-10-06 11:14:27 +0900

    変数のデータを取り出す方法

--- views/SQL.php
+++ views/SQL.php
@@ -2,8 +2,11 @@
 // データベースを繋ぐときのコード,passワード入れてdbname先を指定すると表示される
     $pdo =new PDO('mysql:host=localhost;dbname=paiza;charset=utf8','root','root');
 // SQL文を書いてデータを取り出す*は全てなのでそこの値を変えてみる、WHEREの後にどこから取り出すか書く
-    $sql ='SELECT * FROM players WHERE level >= 7';
+    $sql ='SELECT * FROM players WHERE level >= :lower';
     $statement =$pdo ->prepare($sql);
+    // レベル5以上を映し出すコード
+    $low_value =3;
+    $statement ->bindValue(':lower', $low_value, PDO::PARAM_INT);
     $statement ->execute();
 
     $results =[];
