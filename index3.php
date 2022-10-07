497c458396992e6f6dd4ae252fb68c8d2cd5df5b
sari7happy <sari7happy@gmail.com>
2022-10-07 16:30:15 +0900

    文字列の置換

--- index3.php
+++ index3.php
@@ -1,9 +1,14 @@
 <?php
 // 文字列のながさ
-$text ='abcdefg';
+$text ='あいうえお';
 
-echo strlen($text);
+// echo strlen($text);
 
+echo mb_strlen($text);
+
+// 文字列を置換する機能
+$str = '文字列を田中します';
+echo str_replace('田中','たなか',$str);
 
 
 ?>
