bc566294c8d0bdd7c2b1b2f131274d5b6d899f00
sari7happy <sari7happy@gmail.com>
2022-10-07 14:57:09 +0900

    ANDとORについて

--- index3.php
+++ index3.php
@@ -1,48 +1,19 @@
 <?php
-// /==一致、＝＝＝型も一致
 
 
-$height = 91;
-
-// if($height >= 91){
-//     echo '身長は'.$height.'cmです。';
-// }
-    if($height !== 90){ 
-        // ＝＝で方が同じかどうか
-        echo '身長は90cmではありません。';
-}
-    
-
-// データが入っているかどうか
-$test ='11';
-if(!empty($test)){
-    echo '変数はからではありません';
+// AND ORについて
+$signal_1 ='red';
+$signal_2 = 'yello';
+// AND
+if($signal_1  === 'red' && $signal_2 ==='blue'){
+    echo '赤と青です';
 }
 
+// OR
+if($signal_1  === 'red' ||$signal_2 ==='blue'){
+    echo '赤です';
+}
 
-// $signal ='yello';
-
-// if($signal === 'red'){
-//     echo '止まれ';
-// }
-
-// if($signal ==='yello'){
-//     echo '一旦停止';
-// }
-
-// if($signal === 'blue'){
-//     echo '進む';
-// }  
-
-// $speed = 90;
-
-// if ($speed >='90'){
-//     echo 'スピード違反';
-// }
-
-// if($speed <='80'){
-//     echo '注意喚起';
-// }
 
 
 ?>
