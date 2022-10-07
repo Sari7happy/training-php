17f348a3a11931de5b88f0760a7fdc75dc28630b
sari7happy <sari7happy@gmail.com>
2022-10-07 11:10:36 +0900

    配列

--- index2.php
+++ index2.php
@@ -1,9 +1,23 @@
 <?php
-// 定数は変わらない文字,constはconstantは大文字でかく
 
-const MAX =10;
+// 配列は横一列に1,2,3で表す
+// 配列はarrayという意味で[]で表す,0から始まるので0は１になる
+// 配列１行version
+$array_1 =['あああ',2,3];
 
-echo MAX;
+// 配列2,3行version
+$array_2 = [
+    ['あか','あお','きいろ'],
+    ['みどり','むらさき','くろ']
+];
+
+echo '<pre>';
+var_dump($array_2);
+echo '</pre>';
+// var_dumpで変数全て表示されるecho '<pre>'で縦表示
+
+// array[][];でその文字を指定
+echo $array_2[0][1];
 
 
 
