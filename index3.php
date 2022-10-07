893a11fc06763781df58a00eaaf911207062ebf7
sari7happy <sari7happy@gmail.com>
2022-10-07 14:49:34 +0900

    emptyについて

--- index3.php
+++ index3.php
@@ -2,35 +2,47 @@
 // /==一致、＝＝＝型も一致
 
 
-$height = 90;
+$height = 91;
 
-if($height >= 91){
-    echo '身長は'.$height.'cmです。';
-}
-    if($height <= 90){
-        echo '身長は'.$height.'cmなります。';
+// if($height >= 91){
+//     echo '身長は'.$height.'cmです。';
+// }
+    if($height !== 90){ 
+        // ＝＝で方が同じかどうか
+        echo '身長は90cmではありません。';
 }
     
 
-
-$signal ='blue';
-
-if($signal === 'red'){
-    echo '止まれ';
-}else if($signal ==='yello'){
-    echo '一旦停止';
-
-    echo '<br>';
-
-}else{
-    echo '進む';
+// データが入っているかどうか
+$test ='11';
+if(!empty($test)){
+    echo '変数はからではありません';
 }
-$speed = 80;
 
-if ($signal ==='blue'){
-    if($speed >=90){
-        echo 'スピード違反';
-    }
-}
+
+// $signal ='yello';
+
+// if($signal === 'red'){
+//     echo '止まれ';
+// }
+
+// if($signal ==='yello'){
+//     echo '一旦停止';
+// }
+
+// if($signal === 'blue'){
+//     echo '進む';
+// }  
+
+// $speed = 90;
+
+// if ($speed >='90'){
+//     echo 'スピード違反';
+// }
+
+// if($speed <='80'){
+//     echo '注意喚起';
+// }
+
 
 ?>
