68ccdb67fc70e70b0572687935646cdda7fcf789
sari7happy <sari7happy@gmail.com>
2022-10-07 10:37:45 +0900

    2つの文字の連結

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
     // 変数は後から付け足した方を表示、intは数字、stringは文字静的型付はjavaなど
-    $test =123;
+    $test_1=123;
+    $test_2='テストです';
+// 連結はピリオドで繋ぐ場合は文字として扱われる
+    $test_3 = $test_1. $test_2;
+    // $_test =456;
+    // 英文字か_が先頭
     // $test ='テストです';
-    var_dump($test);
+    var_dump($test_3);
     //動的を調べるにはvar_dump();で調べる
 
     // echo $test;
