e8e8fa83681e6ce8a30eb312d9ea6495b20c7e68
sari7happy <sari7happy@gmail.com>
2022-10-07 14:34:33 +0900

    if文復習

--- index3.php
+++ index3.php
@@ -1,10 +1,36 @@
 <?php
-$test_1 = 2;
-$test_2 = 3;
+// /==一致、＝＝＝型も一致
 
-$test_3 = $test_1 % $test_2;
 
-echo $test_3;
+$height = 90;
 
+if($height >= 91){
+    echo '身長は'.$height.'cmです。';
+}
+    if($height <= 90){
+        echo '身長は'.$height.'cmなります。';
+}
+    
+
+
+$signal ='blue';
+
+if($signal === 'red'){
+    echo '止まれ';
+}else if($signal ==='yello'){
+    echo '一旦停止';
+
+    echo '<br>';
+
+}else{
+    echo '進む';
+}
+$speed = 80;
+
+if ($signal ==='blue'){
+    if($speed >=90){
+        echo 'スピード違反';
+    }
+}
 
 ?>
