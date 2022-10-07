0b00999ee4ff6b8c2f633e698f0212015f71a59b
sari7happy <sari7happy@gmail.com>
2022-10-07 16:23:23 +0900

    文字列の長さわかる関数

--- index3.php
+++ index3.php
@@ -1,34 +1,9 @@
 <?php
-// インプット引数なしアウトプット引数なし
-function test (){
-    echo 'テスト';
-}
+// 文字列のながさ
+$text ='abcdefg';
 
-test();
+echo strlen($text);
 
 
-$comment ='コメント2';
-$comment3 ='コメント3';
-// インプット引数ありアウトプット引数なし
-function getComment($string){
-    echo $string;
-}
-getComment($comment3);
-
-
-// インプット引数なしアウトプット引数あり
-
-function getNumberOfComment(){
-    return 5;
-}
-  echo getNumberOfComment();
-
-// 引数2つ,戻り値あり
-function sumPrice($int1,$int2){
-    $int3 = $int1 + $int2;
-    return $int3;
-}
-$total = sumPrice(3,5);
-echo $total;
 
 ?>
